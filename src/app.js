a9cd00cae73bbd38b4ddabd4c611d4ce0a1cea2b
Diribar <diegoiribarren2015@gmail.com>
2021-02-01 00:11:18 -0300

    Login actualizado al 31-ene-20

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.listen(3010, () => console.log('Servidor funcionando...'))
+app.listen(3000, () => console.log('Servidor funcionando...'))
 let path = require('path')
 app.use(express.static(path.resolve('public')));
 
