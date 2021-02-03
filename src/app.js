8dd354835b939593196e357d2b37e49e04964137
Diribar <76135160+Diribar@users.noreply.github.com>
2021-02-02 21:24:18 -0300

    Carrito de compras y login (#3)
    Vistas de carrito y login

--- src/app.js
+++ src/app.js
@@ -6,8 +6,16 @@ const staticFolder = path.resolve(__dirname, "./public");
 
 app.use(express.static(staticFolder));
 
-app.listen(3000);
+app.listen(3000, () => console.log('Servidor funcionando...'));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/index.html'))
+});
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/carrito.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'))
 });
