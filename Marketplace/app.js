7a66f0594968ed304b6438f626ed2db8eba03ae1
christianduic <christianduic@hotmail.com>
2021-03-03 20:17:26 -0300

    Remove duplicated code

--- Marketplace/app.js
+++ Marketplace/app.js
@@ -15,7 +15,6 @@ app.set('views', [
                     path.resolve(__dirname, './views/products'), 
                     path.resolve(__dirname, './views/user')
                 ]);
-app.use(methodOverride('_method'));
 
 app.listen(3000, () => console.log('Servidor funcionando en puerto 3000...'));
 
