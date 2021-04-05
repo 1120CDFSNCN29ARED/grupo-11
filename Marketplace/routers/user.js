f1616244051e15e02bd454cb03ee78259cbc0684
Diribar <76135160+Diribar@users.noreply.github.com>
2021-04-05 10:50:40 -0300

    Corrección-de-nombres-de-middlewares

--- Marketplace/routers/user.js
+++ Marketplace/routers/user.js
@@ -7,18 +7,18 @@ const userController = require(path.join(__dirname,'../controllers/userControlle
 const uploadFile = require(path.join(__dirname,'../middlewares/multerUsuario'));
 const validarUsuario = require(path.join(__dirname,'../middlewares/validarUsuario'));
 const validarLogin = require(path.join(__dirname,'../middlewares/validarLogin'));
-const avanzaSiNoEstasLogueado = require('../middlewares/avanzaSiNoEstasLogueado');
-const avanzaSiYaEstasLogueado = require('../middlewares/avanzaSiYaEstasLogueado');
+const soloVisitas = require('../middlewares/soloVisitas');
+const soloUsuarios = require('../middlewares/soloUsuarios');
 
 // Rutas
-router.get('/crear', avanzaSiNoEstasLogueado, userController.crearForm);
+router.get('/crear', soloVisitas, userController.crearForm);
 router.post('/crear', uploadFile.single("imagen"), validarUsuario, userController.crearGuardar);
-router.get('/detalle', avanzaSiYaEstasLogueado, userController.detalle);
-router.get('/editar', avanzaSiYaEstasLogueado, userController.editarForm);
+router.get('/detalle', soloUsuarios, userController.detalle);
+router.get('/editar', soloUsuarios, userController.editarForm);
 router.put('/editar', uploadFile.single("imagen"), validarUsuario, userController.editarGuardar);
-router.delete('/eliminar', avanzaSiYaEstasLogueado, userController.eliminar);
-router.get('/login', avanzaSiNoEstasLogueado, userController.login);
+router.delete('/eliminar', soloUsuarios, userController.eliminar);
+router.get('/login', soloVisitas, userController.login);
 router.post('/login', validarLogin, userController.logeo);
-router.get('/logout', avanzaSiYaEstasLogueado, userController.logout);
+router.get('/logout', soloUsuarios, userController.logout);
 
 module.exports = router;
