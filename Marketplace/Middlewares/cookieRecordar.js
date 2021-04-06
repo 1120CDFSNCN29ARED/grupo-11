cbe75ac3244f8ba58c35924eba06019e6128130c
Diribar <76135160+Diribar@users.noreply.github.com>
2021-04-06 09:33:11 -0300

    Error del middleware ya resuelto

--- Marketplace/Middlewares/cookieRecordar.js
+++ Marketplace/Middlewares/cookieRecordar.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 let path = require("path");
 const usersFilePath = path.join(__dirname, "../database/usuarios.json");
 
-function cookieRecordar(req, res, next){
-   if(req.cookies.recordar != undefined && req.session.usuariologeado == undefined){
+module.exports = (req, res, next) => {
+   if(req.cookies && req.cookies.recordar && !req.session.usuariologeado){
       let usuarios = GetFileObject(usersFilePath);
       let usuarioALogearse;
       usuarioALogearse = usuarios.find(n => n.email == req.cookies.recordar);
@@ -12,8 +12,6 @@ function cookieRecordar(req, res, next){
    next();
 }
 
-module.exports = cookieRecordar;
-
 function GetFileObject(filePath) {
    return JSON.parse(fs.readFileSync(filePath, "utf-8"))
-};
+}
