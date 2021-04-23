a2cbccbb620953ae569145f060bf114094453edb
christianduic <christianduic@hotmail.com>
2021-04-23 08:49:35 -0300

    Add repositories

---
+++ Marketplace/repositories/productRepository.js
@@ -0,0 +1,5 @@
+const db = require("../database/models");
+const entidad = db.Productos;
+
+module.exports = {
+};
---
+++ Marketplace/repositories/userRepository.js
@@ -0,0 +1,5 @@
+const db = require("../database/models");
+const entidad = db.Usuarios;
+
+module.exports = {
+};
