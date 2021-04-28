96a5afca3bed2838c1ef35b7aca5ca0f072e7f6d
Diribar <76135160+Diribar@users.noreply.github.com>
2021-04-28 10:20:46 -0300

    Cambiar-sangria-de-espacios-por-tabulaciones

--- Marketplace/Middlewares/validarLogin.js
+++ Marketplace/Middlewares/validarLogin.js
@@ -1,11 +1,11 @@
 const { body } = require("express-validator");
 
 module.exports = [
-    body("email")
-        .notEmpty().withMessage("Tienes que escribir un correo electrónico").bail()
-        .isEmail().withMessage("Formato inválido de correo electrónico").bail()
+	body("email")
+		.notEmpty().withMessage("Tienes que escribir un correo electrónico").bail()
+		.isEmail().withMessage("Formato inválido de correo electrónico").bail()
 		,
-    body("contrasena")
-        .notEmpty().withMessage("Tienes que escribir una contraseña").bail()
+	body("contrasena")
+		.notEmpty().withMessage("Tienes que escribir una contraseña").bail()
 		,
 ];
