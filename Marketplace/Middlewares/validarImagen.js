034de138f616a13ee6adcb1c3e304a5f9016c179
Diribar <76135160+Diribar@users.noreply.github.com>
2021-04-30 08:42:48 -0300

    Indentacion-con-Tabulacion-en-vez-de-con-Espacios

--- Marketplace/Middlewares/validarImagen.js
+++ Marketplace/Middlewares/validarImagen.js
@@ -2,21 +2,21 @@ const path = require("path");
 const { body } = require("express-validator");
 
 module.exports = [
-    body("imagen").custom((value, { req }) => {
-        let file = req.file;
-        let acceptedExtensions = [".jpg", ".png", ".gif", ".bmp"];
-        if (!file) {
-            throw new Error("Tienes que subir una imagen");
-        } else {
-            let fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(
-                    `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
-                        ", "
-                    )}`
-                );
-            }
-        }
-        return true;
-    }),
+	body("imagen").custom((value, { req }) => {
+		let file = req.file;
+		let acceptedExtensions = [".jpg", ".png", ".gif", ".bmp"];
+		if (!file) {
+			throw new Error("Tienes que subir una imagen");
+		} else {
+			let fileExtension = path.extname(file.originalname);
+			if (!acceptedExtensions.includes(fileExtension)) {
+				throw new Error(
+					`Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+						", "
+					)}`
+				);
+			}
+		}
+		return true;
+	}),
 ];
--- Marketplace/database/models/Marca.js
+++ Marketplace/database/models/Marca.js
@@ -1,28 +1,28 @@
 const Sequelize = require("sequelize");
 
 module.exports = (sequelize) => {
-    const alias = "Marca";
-    const columns = {
-        nombre: Sequelize.STRING(500)
-    };
-    const config = {
-        tableName: "marcas",
-        timestamps: false
-    };
+	const alias = "Marca";
+	const columns = {
+		nombre: Sequelize.STRING(500)
+	};
+	const config = {
+		tableName: "marcas",
+		timestamps: false
+	};
 
-    const Marca = sequelize.define(alias,columns,config);
+	const Marca = sequelize.define(alias,columns,config);
 
-    Marca.associate = function(models) {
-        Marca.hasMany(models.Modelo, {
-            as: "modelos",
-            foreignKey: "marca_id"
-        });
-        
-        Marca.hasMany(models.Producto, {
-            as: "productos",
-            foreignKey: "marca_id"
-        });
-    };
+	Marca.associate = function(models) {
+		Marca.hasMany(models.Modelo, {
+			as: "modelos",
+			foreignKey: "marca_id"
+		});
+		
+		Marca.hasMany(models.Producto, {
+			as: "productos",
+			foreignKey: "marca_id"
+		});
+	};
 
-    return Marca;
+	return Marca;
 };
