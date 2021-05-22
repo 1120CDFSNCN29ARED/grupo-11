a9f99f718312739289e3762ebb8bec1c94c8b866
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-22 19:48:25 -0300

    Miscelaneas

--- Marketplace/controllers/api/apiProductController.js
+++ Marketplace/controllers/api/apiProductController.js
@@ -11,14 +11,14 @@ module.exports = {
 			let aux=[]
 			n.imagenes.map(m => aux.push(m.ruta));
 			productos.push({
-                id: n.id,
-                nombre: n.nombre,
-                descripcion: n.descripcion,
-                categoria: n.categoria.nombre,
-                imagenes: n.imagenes.map((m) => m.ruta),
-                url: "/api/productos/" + n.id,
-                creado: n.creado_en,
-            });
+				id: n.id,
+				nombre: n.nombre,
+				descripcion: n.descripcion,
+				categoria: n.categoria.nombre,
+				imagenes: n.imagenes.map((m) => m.ruta),
+				url: "/api/productos/" + n.id,
+				creado: n.creado_en,
+			});
 		});
 		let resumenProductos = {
 			cantidad: data.length,
@@ -42,10 +42,10 @@ module.exports = {
 			totales[n.nombre] = n.productos.length
 		});
 		let resumenCategorias = {
-            cantidad: data.length,
-            detalle: categorias,
-            totales: totales,
-        };
+			cantidad: data.length,
+			detalle: categorias,
+			totales: totales,
+		};
 		// *** FINAL ***
 		let respuesta = {
 			productos: resumenProductos,
@@ -59,16 +59,16 @@ module.exports = {
 		data = await productoRepository.ObtenerPorId(req.params.id);
 		// *** Proceso de la info ***
 		let producto = {
-            id: data.id,
-            nombre: data.nombre,
-            descripcion: data.descripcion,
-            categoria: data.categoria.nombre,
-            marca: data.marcas.nombre,
-            modelo: data.modelos.nombre,
-            precio: data.precio,
-            stock: data.stock_disponible,
-            imagenes: data.imagenes.map(n => n.ruta),
-        };
+			id: data.id,
+			nombre: data.nombre,
+			descripcion: data.descripcion,
+			categoria: data.categoria.nombre,
+			marca: data.marcas.nombre,
+			modelo: data.modelos.nombre,
+			precio: data.precio,
+			stock: data.stock_disponible,
+			imagenes: data.imagenes.map(n => n.ruta),
+		};
 		res.json(producto);
 	},
 }
