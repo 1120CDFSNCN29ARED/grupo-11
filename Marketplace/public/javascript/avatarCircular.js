06e05d06fc6c4874fef1ba076f790c312b8291e4
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-24 13:34:46 -0300

    Miscelaneas

--- Marketplace/public/javascript/avatarCircular.js
+++ Marketplace/public/javascript/avatarCircular.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
 	let avatar = document.getElementById("imagenUsuario");
 	let ancho =  avatar.naturalWidth
-    let alto = avatar.naturalHeight
+	let alto = avatar.naturalHeight
 	
 	if (ancho > alto) {avatar.classList.add("imagenAncha")} else
 	if (ancho < alto) {avatar.classList.add("imagenAlta")} else
