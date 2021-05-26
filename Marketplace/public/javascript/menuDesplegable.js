c42ff95f31929e0b06ae7fd5c91a8d1d2db24752
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-26 17:20:17 -0300

    Solucionada la funcionalidad del menú desplegable

--- Marketplace/public/javascript/menuDesplegable.js
+++ Marketplace/public/javascript/menuDesplegable.js
@@ -2,11 +2,11 @@
 function desplegable(n) {document.getElementById(n).classList.toggle("mostrar");}
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-	if (!event.target.matches('.boton-desplegable')) {
-	let dropdowns = document.getElementsByClassName("dropdown-content");
-	for (let n of dropdowns) {
-		n.classList.remove('mostrar');
-	}
+window.onclick = function(e) {
+	if (!e.target.matches('.boton-desplegable')) {
+		let dropdowns = document.getElementsByClassName("dropdown-content");
+		for (let n of dropdowns) {
+			n.classList.remove('mostrar');
+		}
 	}
 }
