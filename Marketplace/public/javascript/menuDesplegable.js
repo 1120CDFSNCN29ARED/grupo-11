52eb8cf0d7d0c9f7d9f2e03edef553bd5b9eba51
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-27 01:35:40 -0300

    onclicks de Header llevados al archivo menuDesplegable.js

--- Marketplace/public/javascript/menuDesplegable.js
+++ Marketplace/public/javascript/menuDesplegable.js
@@ -1,12 +1,20 @@
-// When the user clicks on the button, toggle between hiding and showing the dropdown content
-function desplegable(n) {document.getElementById(n).classList.toggle("mostrar");}
+window.addEventListener("load", () => {
+	document
+		.getElementById("hamburguer-container")
+		.addEventListener('click', () => {
+			document.getElementById('menu-categorias').classList.toggle("mostrar")
+		})
+	document
+		.getElementById("profile-container")
+		.addEventListener("click", () => {
+			document.getElementById("menu-usuario").classList.toggle("mostrar");
+		});
+});
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-	if (!e.target.matches('.boton-desplegable')) {
-		let dropdowns = document.getElementsByClassName("dropdown-content");
-		for (let n of dropdowns) {
-			n.classList.remove('mostrar');
-		}
-	}
-}
+// Cerrar los dropdowns en desuso
+window.onclick = function (e) {
+	!e.target.matches("#categoria-button") ? document.getElementById("menu-categorias").classList.remove("mostrar") : ""
+	!e.target.matches(".desplegableUsuario") ? document.getElementById("menu-usuario").classList.remove("mostrar") : ""
+};
+
+function desplegable(n) {document.getElementById(n).classList.toggle("mostrar")}
