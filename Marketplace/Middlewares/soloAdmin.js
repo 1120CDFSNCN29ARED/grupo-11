5f8a11c811d36ff9504c808af7de03dcb01fdc97
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-30 19:18:39 -0300

    Error corregido en middleware Admin

--- Marketplace/Middlewares/soloAdmin.js
+++ Marketplace/Middlewares/soloAdmin.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
-	if (!req.session.usuarioLogeado && req.session.usuarioLogeado.rol_id !=1 ) {
+	if (!req.session.usuarioLogeado) {
+		return res.redirect("/usuario/login");
+	} else
+	if (req.session.usuarioLogeado.rol_id !=1 ) {
 		return res.redirect("/usuario/logout");
 	}
 	next();
