0361d7dc29649370c3af055fa8da6d2c6e4ee834
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-30 20:18:19 -0300

    Mejora en la configuracion vertical del avatar 'alto'

--- Marketplace/public/javascript/avatarCircular.js
+++ Marketplace/public/javascript/avatarCircular.js
@@ -4,7 +4,12 @@ window.addEventListener("load", () => {
 	let alto = avatar.naturalHeight
 	
 	if (ancho > alto) {avatar.classList.add("imagenAncha")} else
-	if (ancho < alto) {avatar.classList.add("imagenAlta")} else
+	if (ancho < alto) {
+		avatar.classList.add("imagenAlta")
+		let contenedor = document.getElementById("profile-container")
+		contenedor.style.flexDirection = "column"
+		contenedor.style.justifyContent = "flex-start";
+	} else
 	avatar.classList.add("imagenCuadrada");
 	
 })
