f40f56ba030a916ff67987dd7c5e09ec1688154f
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-31 15:20:46 -0300

    GS79-Editar-el-carrito

---
+++ Marketplace/public/javascript/carrito.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", () => {
+	// Declarar lasvariables
+	let cantidad = document.querySelectorAll("#cantidad");
+	let precio = document.querySelectorAll("#precio");
+	let importe = document.querySelector("#importe");
+	let eliminar = document.querySelectorAll("#eliminar");
+	let registroID = document.querySelectorAll("#registroID");
+
+	// Rutinas por cada registro
+	for (let i = 0; i < cantidad.length; i++) {
+		// Cambios en la cantidad
+		cantidad[i].addEventListener("input", () => {
+			cant = parseInt(cantidad[i].value);
+			console.log(cantidad)
+			cant < 0 ? (cant = 0) : "";
+			actualizar(cant, i);
+		});
+		// Eliminar el registro
+		eliminar[i].addEventListener("click", () => {
+			location = "/carrito/borrar-registro/" + registroID[i].innerHTML;
+		});
+	}
+
+	// Rutina para el encabezado
+	window.addEventListener("click", () => {
+		let acumulador = 0;
+		for (let i = 0; i < cantidad.length; i++) {
+			cant = parseInt(cantidad[i].value);
+			price = parseInt(precio[i].innerHTML);
+			acumulador = acumulador + cant * price;
+		}
+		importe.innerHTML = "$ " + toThousand(acumulador);
+	});
+});
+
+// Funciones
+function actualizar(cant, i) {
+	let cantidad = document.querySelectorAll("#cantidad");
+	let precio = document.querySelectorAll("#precio");
+	let valorTotal = document.querySelectorAll("#valorTotal");
+	cantidad[i].value = cant;
+	price = parseInt(precio[i].innerHTML);
+	importe = cant * price;
+	valorTotal[i].innerHTML = "$ " + toThousand(importe);
+}
+
+function toThousand(n) {
+	return parseInt(n).toLocaleString("es-AR", { maximumFractionDigits: 0 });
+}
--- Marketplace/routers/main.js
+++ Marketplace/routers/main.js
@@ -2,11 +2,7 @@ const express = require("express");
 const mainController = require("../controllers/mainController");
 const router = express.Router();
 
-// Middlewares
-const soloUsuarios = require("../middlewares/soloUsuarios");
-
 // Rutas
 router.get("/", mainController.index);
-router.get("/carrito", soloUsuarios, mainController.carrito);
 
 module.exports = router;
