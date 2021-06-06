e327484aa2b9dfb5f408c07e956acf33903b9085
Diribar <76135160+Diribar@users.noreply.github.com>
2021-06-06 14:08:32 -0300

    Se agrregó un 'if' al contador, para evitar un mensaje de error en la consola cuando el usuario no está logueado

---
+++ Marketplace/controllers/api/apiCarritoController.js
@@ -0,0 +1,17 @@
+// Requires ***********************************
+const carritoRepository = require("../../repositories/carritoRepository");
+
+module.exports = {
+
+	contador: async (req, res) => {
+		if (req.session && req.session.usuarioLogeado) {
+			let usuarioID = req.session.usuarioLogeado.id;
+			let contador = await carritoRepository
+				.ObtenerTodos(usuarioID)
+				.then((n) => n.length.toString());
+			return res.json(contador);
+		} else {
+			return res.json(null);
+		}
+	},
+};
